0a3c4a8b30e2d4323d07ff6ab480f96115223d25
David <david@tximeneafilms.com>
2026-06-19 13:46:37 +0200

    Fix carousel motion.a href

--- components/carrousel/carrousel.js
+++ components/carrousel/carrousel.js
@@ -44,22 +44,21 @@ function Carrousel({pages}) {
             }}
           >{page.title}</motion.h1>
           <div style={{zIndex:1}}>
-            <Link href={"/projects/" + page.link} legacyBehavior>
-              <motion.a
-                key={currentPage + "_desktop"}
-                custom={direction}
-                variants={variants}
-                initial="enter"
-                animate="center"
-                exit={{ scale: 2 }}
-                transition={{
-                  x: { type: "spring", stiffness: 300, damping: 30 },
-                  opacity: { duration: 0.2 },
-                  scale: { duration: 3 },
-                }}>
-                <img src={p(page.image)} alt={"image"} className={styles.carrousel_desktop_image} width={750} height={450} key={page.image} />
-              </motion.a>
-            </Link>
+            <motion.a
+              href={p("/projects/" + page.link)}
+              key={currentPage + "_desktop"}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit={{ scale: 2 }}
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+                scale: { duration: 3 },
+              }}>
+              <img src={p(page.image)} alt={"image"} className={styles.carrousel_desktop_image} width={750} height={450} key={page.image} />
+            </motion.a>
             <div className={styles.carrousel_desktop_pageDisplay}>
                   <div className={currentPage === 0 ? styles.carrousel_desktop_pageDisplay_arrow_noDisplay : `${styles.carrousel_desktop_pageDisplay_arrow} ${styles.carrousel_desktop_pageDisplay_arrow_back}`} onClick={() => paginate(-1)} ></div>
                   {pages.map((page, index) => <div className={ currentPage != index ? styles.carrousel_desktop_pageDisplay_item : `${styles.carrousel_desktop_pageDisplay_item} ${styles.carrousel_desktop_pageDisplay_item_active}` } key={ "page_" + index } onClick={() => setPage([index,1])} ></div>)}
